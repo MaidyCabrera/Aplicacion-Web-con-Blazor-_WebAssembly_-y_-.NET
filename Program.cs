5fc7c8c9c1cabce87229e9e8afaf71af71c1c6fa
Maidy Cabrera <m.cabrerap@uniandes.edu.co>
2023-06-27 12:12:00 -0500

    Feature: Update project

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using blazorappdemo;
+using Blazored.Toast;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 var apiUrl = builder.Configuration.GetValue<string>("apiUrl");
+
+//Libreria externa
+builder.Services.AddBlazoredToast();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl) });
 builder.Services.AddScoped<IProductService, ProductService>();
